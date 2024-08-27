9ef5d9f9ae3e6f71eaa4254140a4009449104888
Kajolsingh123 <kajolkumari047@gmail.com>
2024-08-27 23:29:46 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,17 +1,5 @@
-
-
-const Parent = React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"} , [
-        React.createElement("h1",{id:"child2"},"I'm H1 Tag" ),
-        React.createElement("h2",{id:"child2"},"I'm H2 Tag" ),
-        React.createElement("h3",{id:"child2"},"I'm H3 Tag" ) 
-    ]),
-    React.createElement("div",{id:"child"} , [
-        React.createElement("h1",{id:"child2"},"I'm H1 Tag" ),
-        React.createElement("h2",{id:"child2"},"I'm H2 Tag" ),
-        React.createElement("h3",{id:"child2"},"I'm H3 Tag" ) 
-    ])
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 
@@ -20,8 +8,5 @@ const Parent = React.createElement("div",{id:"parent"},
 
 
 
-const heading = React.createElement("h1", {id:"heading"},"Hello World from React");
-     
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(Parent)
+
