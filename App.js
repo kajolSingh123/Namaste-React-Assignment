e608bdc32fb05d45ab2e99ad2fd433a3b7de98ed
Kajolsingh123 <kajolkumari047@gmail.com>
2024-08-28 15:59:37 +0530

    episode-3 Functional component

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
-const Titel = () => (
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <h1 className="heading">React Functional Component</h1>
+    </div>
+  );
+};
+const titel = () => (
   <h1 className="head" tabIndex="6">
     Hello world
+    <HeadingComponent />
   </h1>
 );
 
-// Component Composition in React
-
-const HeadingComponent = () => {
-  return (
-    <>
-      <Titel />
-      <h1 className="heading">React Functional Component</h1>
-    </>
-  );
-};
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(titel);
