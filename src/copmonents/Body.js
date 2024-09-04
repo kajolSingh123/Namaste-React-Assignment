5d0f7a7ea97e828900f0feea85b46eb93a8980ee
Kajolsingh123 <kajolkumari047@gmail.com>
2024-09-04 17:23:49 +0530

    episode-6

--- src/copmonents/Body.js
+++ src/copmonents/Body.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [searchText, setSearchtext] = useState("");
   useEffect(() => {
     fetchData();
@@ -15,27 +16,38 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    //  Json data
-    // console.log(json);
-    // Optional Chaining to use ?
+
     setListOfRestaurants(
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+    setFilteredRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
-  //Conditional Rendering -
-  // if (listOfRestaurants.length === 0) {
-  //   return <Shimmer />;
-  // }
-
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
       <div className="filter">
         <div className="search">
-          <input type="text" className="search-box" value={searchText}></input>
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => {
+              setSearchtext(e.target.value);
+            }}></input>
 
-          <button onClick={() => {}}>Search</button>
+          <button
+            onClick={() => {
+              const filteredRestaurants = listOfRestaurants.filter((res) => {
+                return res.info.name
+                  .toLocaleLowerCase()
+                  .includes(searchText.toLocaleLowerCase());
+              });
+              setFilteredRestaurants(filteredRestaurants);
+            }}>
+            Search
+          </button>
           {/* Fillter the Restaurant Cards and update the UI  */}
         </div>
         <button
@@ -50,7 +62,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-containner">
-        {listOfRestaurants.map((items) => (
+        {filteredRestaurants.map((items) => (
           <RestaurantCard key={items.info.id} resData={items} />
         ))}
       </div>
